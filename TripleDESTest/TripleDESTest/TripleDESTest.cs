b8772021c7fef0eec507f14dec6121dda53985b4
cheong00 <cheong00@outlook.com>
2017-08-17 22:43:22 +0900

    Initial commit to TripleDESTest project.

---
+++ TripleDESTest/TripleDESTest/TripleDESTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace TripleDESTest
+{
+    class TripleDESTest
+    {
+        static void Main(string[] args)
+        {
+            string myData = "This is a test.";
+            string myKey = "5137153cc611227c000bbd1bd8cd200";//Development Env Key.
+            string encrypted = EncryptTDES(myData, myKey);
+            Console.Write("Encrypted string = ");
+            Console.WriteLine(encrypted);
+            //Target: oHX1DB5B0rvn6mlr9T7pkg==
+
+            string s = DecryptTDES(encrypted, myKey);
+            Console.Write("Decrypted string = ");
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+
+        public static string EncryptTDES(string value, string decryptionKey)
+        {
+            TripleDESCryptoServiceProvider tDESalg = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+            string result = null;
+            try
+            {
+                byte[] encryptdata = Encoding.UTF8.GetBytes(value);
+
+                tDESalg.Mode = CipherMode.ECB;
+                tDESalg.Padding = PaddingMode.PKCS7;//According to MS, same as PKCS5PADDING
+
+                byte[] hash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(decryptionKey));
+                byte[] Key = new byte[tDESalg.KeySize / 8];
+                Array.Copy(hash, Key, hash.Length < Key.Length ? hash.Length : Key.Length); //The size of the Key property must be the same as the KeySize property divided by 8
+
+                tDESalg.Key = Key;
+                ICryptoTransform cryptoTransform = tDESalg.CreateEncryptor();
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(encryptdata, 0, encryptdata.Length);
+                        cryptoStream.FlushFinalBlock();
+                        result = Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message + e.StackTrace);
+                return null;
+            }
+            return result;
+        }
+
+        public static string DecryptTDES(string value, string decryptionKey)
+        {
+            string decryptString = "";
+            TripleDESCryptoServiceProvider tDESalg = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+            try
+            {
+                byte[] decodedData = Convert.FromBase64String(value);
+                tDESalg.Mode = CipherMode.ECB;
+                tDESalg.Padding = PaddingMode.PKCS7;//According to MS, same as PKCS5PADDING
+
+                byte[] hash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(decryptionKey));
+                byte[] Key = new byte[tDESalg.KeySize / 8]; //The size of the Key property must be the same as the KeySize property divided by 8
+                Array.Copy(hash, Key, hash.Length < Key.Length ? hash.Length : Key.Length);
+
+                tDESalg.Key = Key;
+                ICryptoTransform cryptoTransform = tDESalg.CreateDecryptor();
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    using (var cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(decodedData, 0, decodedData.Length);
+                        cryptoStream.FlushFinalBlock();
+                        decryptString = Encoding.UTF8.GetString(memoryStream.ToArray());
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message + e.StackTrace);
+                return null;
+            }
+
+            return decryptString;
+        }
+
+    }
+}
---
+++ TripleDESTest/TripleDESTest/TripleDESTest.java
@@ -0,0 +1,91 @@
+﻿import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import java.security.MessageDigest;
+
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class TripleDESTest {
+
+    public static void main(String[] args) {
+        String text = "This is a test.";
+		String myKey = "5137153cc611227c000bbd1bd8cd200";//Development Env Key.
+
+        String codedtext = encrypt(text, myKey);
+        System.out.print("Encrypted string = ");
+        System.out.println(codedtext);
+        //Target: oHX1DB5B0rvn6mlr9T7pkg==
+
+        String decodedtext  = decrypt(codedtext, myKey);
+        System.out.print("Decrypted string = ");
+        System.out.println(decodedtext);
+    }
+
+    public static String encrypt(String value, String decryptionKey) {
+        try {
+            final MessageDigest md = MessageDigest.getInstance("md5");
+            final byte[] digestOfPassword = md.digest(decryptionKey.getBytes("utf-8"));
+            final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+
+            final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+            //final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+            final Cipher cipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            final byte[] plainTextBytes = value.getBytes("utf-8");
+            final byte[] cipherText = cipher.doFinal(plainTextBytes);
+            final String encodedCipherText = Base64.getEncoder().encodeToString(cipherText);
+
+            return encodedCipherText;    
+        }
+        catch (javax.crypto.NoSuchPaddingException e) { System.out.println("No Such Padding"); }
+        catch (java.security.NoSuchAlgorithmException e) { System.out.println("No Such Algorithm"); }
+        catch (java.security.InvalidKeyException e) { System.out.println("Invalid Key"); }
+        catch (BadPaddingException e) { System.out.println("Invalid Key");}
+        catch (IllegalBlockSizeException e) { System.out.println("Invalid Key");}
+        catch (UnsupportedEncodingException e) { System.out.println("Invalid Key");}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String value, String decryptionKey) {
+        try
+        {
+            final MessageDigest md = MessageDigest.getInstance("md5");
+            final byte[] digestOfPassword = md.digest(decryptionKey.getBytes("utf-8"));
+            final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+
+            final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+            //final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+            final Cipher decipher = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+            decipher.init(Cipher.DECRYPT_MODE, key);
+
+            final byte[] encData = Base64.getDecoder().decode(value);
+            final byte[] plainText = decipher.doFinal(encData);
+
+            return new String(plainText, "UTF-8");            
+        }
+        catch (javax.crypto.NoSuchPaddingException e) { System.out.println("No Such Padding"); }
+        catch (java.security.NoSuchAlgorithmException e) { System.out.println("No Such Algorithm"); }
+        catch (java.security.InvalidKeyException e) { System.out.println("Invalid Key"); }
+        catch (BadPaddingException e) { System.out.println("Invalid Key");}
+        catch (IllegalBlockSizeException e) { System.out.println("Invalid Key");}
+        catch (UnsupportedEncodingException e) { System.out.println("Invalid Key");}     
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+	
+}
