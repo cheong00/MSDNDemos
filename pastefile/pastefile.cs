d4a970cd87a50c09ef29a7e0f9f66ef62d6c57e8
cheong00 <cheong00@outlook.com>
2020-06-10 23:59:23 +0900

    Uploaded single file project to show how to paste files in RDP sessions.
    Updated .gitignore to ignore EXE files generated by "csc.exe".

---
+++ pastefile/pastefile.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/// Just use "csc -reference:%windir%\Microsoft.NET\assembly\GAC_64\PresentationCore\v4.0_4.0.0.0__31bf3856ad364e35\PresentationCore.dll,%windir%\Microsoft.NET\assembly\GAC_MSIL\WindowsBase\v4.0_4.0.0.0__31bf3856ad364e35\WindowsBase.dll VirtualFileDataObject.cs pastefile.cs /out:pastefile.exe" to compile this project
+
+namespace pastefile
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+	
+	class Form1: Form
+	{
+		const string filename = @"C:\Downloads\ASPAJAXExtSetup.msi";
+		FileStream fs = new FileStream(filename, FileMode.Open);
+		
+		private System.ComponentModel.IContainer components = null;
+		private Button btnInvoke = null;
+		
+		protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+		
+		private void Form1_Load(object sender, EventArgs e)
+        {
+			//MessageBox.Show("Load");
+        }
+		
+		private void btnInvoke_Click(object sender, EventArgs e)
+        {
+			//MessageBox.Show("Invoke");
+			try
+			{
+                StringCollection filelist = new StringCollection();
+                filelist.Add(filename);
+                Clipboard.Set​File​Drop​List(filelist);
+            }
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+        }
+		
+		private void InitializeComponent()
+        {
+			this.btnInvoke = new Button();
+			this.SuspendLayout();
+
+			this.btnInvoke.Location = new System.Drawing.Point(5, 5);
+			this.btnInvoke.Size = new System.Drawing.Size(120, 25);
+			this.btnInvoke.Name = "btnInvoke";
+			this.btnInvoke.Text = "Invoke";
+			this.btnInvoke.TabIndex = 0;
+            this.btnInvoke.UseVisualStyleBackColor = true;
+            this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_Click);
+			
+			
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 260);
+            this.Controls.Add(this.btnInvoke);
+            this.Name = "Form1";
+            this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			
+			this.ResumeLayout(false);
+		}
+		
+		public Form1()
+        {
+            InitializeComponent();
+        }
+	}
+}
